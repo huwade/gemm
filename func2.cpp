1afdf851456a3d88cc109807d9d28e5363ae8487
Unknown <family790811@gmail.com>
2018-08-02 14:17:07 +0800

    try to implement im2col
    this is im2col, previous one is conv op

--- func2.cpp
+++ func2.cpp
@@ -13,6 +13,25 @@ enum Padding {
 
 
 
+class OpKernelContext;
+
+
+// Used to keep track of persistent memory buffers used within the op.
+// It uses malloc and free to avoid the time cost of initializing the memory.
+template <class T, size_t size>
+struct Im2ColBufferResource : public ResourceBase {
+  Im2ColBufferResource<T, size>() {
+    data = static_cast<T*>(port::Malloc(size * sizeof(T)));
+  }
+  ~Im2ColBufferResource<T, size>() { port::Free(data); }
+  // This mutex ensures that only a single operation at a time is able to use
+  // the buffer memory held by this resource.
+  
+  //mutex mu;
+  T* data;
+  string DebugString() { return "Im2ColBufferResource"; }
+};
+
 // With that in mind, I've avoided using anything except pretty standard C++
 // types. This is especially noticeable in the data access through raw array
 // indexing. It's deliberate in this case though, since it makes the underlying
@@ -23,7 +42,8 @@ enum Padding {
 // filter_data = [filter_height, filter_width, input_depth, filter_count]
 // output_data = [input_batches, output_height, output_width, filter_count]
 template <class T1, class T2, class T3>
-class ReferenceConvFunctor {
+class ReferenceConvFunctor 
+{
  public:
   void operator()(const T1* input_data,
                   int input_batches, int input_height, int input_width,
@@ -131,9 +151,214 @@ class ReferenceConvFunctor {
 };
 
 
+// Implements convolution as a two stage process, first packing the patches of
+// the input image into columns (im2col) and then running GEMM to produce the
+// final result.
+template <class T1, class T2, class T3, class TGemmFunctor>
+class Im2ColConvFunctor {
+ public:
+  void operator()(OpKernelContext* context, const T1* input_data,
+                  int input_batches, int input_height, int input_width,
+                  int input_depth, const T2* filter_data, int filter_height,
+                  int filter_width, int filter_count, int stride_rows,
+                  int stride_cols, Padding padding, T3* output_data,
+                  int output_height, int output_width) {
+   
+
+    
+
+    // These calculations define how the patches will be positioned within the
+    // input image. The actual definitions are quite complex, and rely on the
+    // previously-calculated output size.
+    int filter_left_offset;
+    int filter_top_offset;
+    if (padding == VALID) {
+      filter_left_offset =
+          ((output_width - 1) * stride_cols + filter_width - input_width + 1) /
+          2;
+      filter_top_offset = ((output_height - 1) * stride_rows + filter_height -
+                           input_height + 1) /
+                          2;
+    } else {
+      filter_left_offset =
+          ((output_width - 1) * stride_cols + filter_width - input_width) / 2;
+      filter_top_offset =
+          ((output_height - 1) * stride_rows + filter_height - input_height) /
+          2;
+    }
+
+    // The im2col buffer has # of patches rows, and # of filters cols.
+    // It's laid out like this, in row major order in memory:
+    //        < filter value count >
+    //   ^   +---------------------+
+    // patch |                     |
+    // count |                     |
+    //   v   +---------------------+
+    // Each patch row contains a filter_width x filter_height patch of the
+    // input, with the depth channel as the most contiguous in memory, followed
+    // by the width, then the height. This is the standard memory order in the
+    // image world if it helps to visualize it.
+    const int filter_value_count = filter_width * filter_height * input_depth;
+
+    const int64 patches_per_chunk =
+        kMaxChunkSize / (filter_value_count * sizeof(T1));
+
+    const int64 chunk_value_count =
+        (kMaxChunkSize + (sizeof(T1) - 1)) / sizeof(T1);
+    // Because memory allocation is very expensive on mobile platforms, try to
+    // allocate a persistent buffer that will be kept around between calls. We
+    // use TensorFlow's resource management to ensure that the memory will be
+    // released when the session is over.
+    Im2ColBufferResource<T1, chunk_value_count>* im2col_buffer_resource;
+    
+    std::function<Status(Im2ColBufferResource<T1, chunk_value_count>**)>
+        creator = [](Im2ColBufferResource<T1, chunk_value_count>** resource) {
+          *resource = new Im2ColBufferResource<T1, chunk_value_count>();
+          return Status::OK();
+        };
+
+    // This means that multiple ops can't be run simultaneously on different
+    // threads, because we have a single shared resource. The platforms this is
+    // aimed at have intra-op parallelism as their focus though, so it shouldn't
+    // be an issue.
+    
+    //mutex_lock lock_buffer(im2col_buffer_resource->mu); ?? can I comment this ?
+    core::ScopedUnref unref_buffer(im2col_buffer_resource);
+    T1* im2col_buffer = im2col_buffer_resource->data;
+
+    const int64 patch_count = (input_batches * output_height * output_width);
+    const int64 chunk_count =
+        (patch_count + (patches_per_chunk - 1)) / patches_per_chunk;
+    for (int64 chunk_index = 0; chunk_index < chunk_count; ++chunk_index) {
+      const int64 patch_index_start = chunk_index * patches_per_chunk;
+      const int64 patch_index_end =
+          std::min(patch_index_start + patches_per_chunk, patch_count);
+      for (int64 patch_index = patch_index_start; patch_index < patch_index_end;
+           ++patch_index) {
+        const int64 batch = patch_index / (output_height * output_width);
+        const int64 out_y = (patch_index / output_width) % output_height;
+        const int64 out_x = patch_index % output_width;
+        const T1* input_batch_start =
+            input_data + (batch * input_height * input_width * input_depth);
+        const int in_y_origin = (out_y * stride_rows) - filter_top_offset;
+        const int in_x_origin = (out_x * stride_cols) - filter_left_offset;
+        const int patch_index_within_chunk = patch_index % patches_per_chunk;
+        T1* im2col_patch_start =
+            im2col_buffer + (patch_index_within_chunk * filter_value_count);
+        for (int filter_y = 0; filter_y < filter_height; ++filter_y) {
+          const int in_y = in_y_origin + filter_y;
+          T1* im2col_row_start =
+              im2col_patch_start + (filter_y * filter_width * input_depth);
+          // If we're off the top or the bottom of the input, fill the
+          // whole row with zeroes.
+          if ((in_y < 0) || (in_y >= input_height)) {
+            T1* im2col_row_end =
+                im2col_row_start + (filter_width * input_depth);
+            std::fill(im2col_row_start, im2col_row_end, T1(0));
+          } else {
+            // What we're doing here is trying to copy and fill the im2col
+            // buffer as efficiently as possible, using functions to set or
+            // duplicate values en masse. We know we don't have to worry about
+            // vertical edges because we dealt with that case above, so we
+            // just need to handle filters that overlap the left or right
+            // edges. Here's what that looks like:
+            //
+            // < left_zero_count > < center_copy_count > < right_zero_count >
+            // +------------------+---------------------+--------------------+
+            // |     (filter)     |       (image)       |      (filter)      |
+            // +------------------+---------------------+--------------------+
+            // in_x_origin        0                 input_width       in_x_end
+            //
+            // In reality it's unlikely that a filter patch will be wider
+            // than an input, but this shows all the edge cases.
+            // We use std::fill() to set the left and right sections to zeroes
+            // and std::copy() to copy over the input data for the center.
+            const int in_x_end = in_x_origin + filter_width;
+            const int left_zero_count = std::max(0, 0 - in_x_origin);
+            const int right_zero_count = std::max(0, in_x_end - input_width);
+            const int center_copy_count =
+                filter_width - (left_zero_count + right_zero_count);
+            if (left_zero_count > 0) {
+              T1* im2col_left_start = im2col_row_start;
+              T1* im2col_left_end =
+                  im2col_left_start + (left_zero_count * input_depth);
+              std::fill(im2col_left_start, im2col_left_end, T1(0));
+            }
+            if (center_copy_count > 0) {
+              const T1* input_row_start =
+                  input_batch_start + (in_y * input_width * input_depth) +
+                  (std::max(0, in_x_origin) * input_depth);
+              const T1* input_row_end =
+                  input_row_start + (center_copy_count * input_depth);
+              T1* im2col_center_start =
+                  im2col_row_start + (left_zero_count * input_depth);
+              std::copy(input_row_start, input_row_end, im2col_center_start);
+            }
+            if (right_zero_count > 0) {
+              T1* im2col_right_start =
+                  im2col_row_start +
+                  ((left_zero_count + center_copy_count) * input_depth);
+              T1* im2col_right_end =
+                  im2col_right_start + (right_zero_count * input_depth);
+              std::fill(im2col_right_start, im2col_right_end, T1(0));
+            }
+          }
+        }
+      }
+      // Now we've assembled a set of image patches into a matrix, apply a
+      // GEMM matrix multiply of the patches as rows, times the filter
+      // weights in columns, to get partial results in the output matrix.
+      const int how_many_patches = patch_index_end - patch_index_start;
+      const int m = how_many_patches;
+      const int n = filter_count;
+      const int k = filter_value_count;
+      const int lda = filter_value_count;
+      const int ldb = filter_count;
+      const int ldc = filter_count;
+      
+      T3* chunk_output_data = output_data + (patch_index_start * filter_count);
+      
+      //TGemmFunctor gemm_functor;
+      //gemm_functor(context, m, n, k, im2col_buffer, lda, filter_data, ldb,
+                   //chunk_output_data, ldc);
+    }
+  }
+};
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {
+    
+    const int depth = 1;
+    const int image_width = 4;
+    const int image_height = 3;
+    const int image_batch_count = 1;
+    // The image matrix is:
+    // |  1 |  2 |  3 |  4 |
+    // |  5 |  6 |  7 |  8 |
+    // |  9 | 10 | 11 | 12 |
+    const int image_data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    
+    // The filter matrix is:
+    // | 1 | 4 | 7 |
+    // | 2 | 5 | 8 |
+    // | 3 | 6 | 9 |
+    const int filter_size = 3;
+    const int filter_count = 1;
+    const int filter_data[] = {1, 4, 7, 2, 5, 8, 3, 6, 9};
+   
+    
+    
+    
     std::cout << "hello" << std::endl;
     return 0;
 }
